00c127eb9b9ccb5a1dbf7fe5f6bf33ff6a8353de
endok <keisuke.endo9@gmail.com>
2013-06-16 16:56:49 +0900

    dotfileのリンクを貼るスクリプト追加。

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+dotfiles="$HOME/.dotfiles"
+
+function link_dotfile() {
+  local from=$1
+  local to=$2
+
+  if [ -L $to ]; then
+    echo "$to already exists."
+    return
+  fi
+
+  if [ -f $to -o -d $to ]; then
+    echo "$to exists. mv $to $to.backup"`date +%Y%m%d%H%M%S`
+    mv $to "$to.backup"`date +%Y%m%d%H%M%S`
+  fi
+
+  echo "ln -s $from $to"
+  ln -s $from $to
+}
+
+link_dotfile "$dotfiles/dot.screenrc" "$HOME/.screenrc"
+link_dotfile "$dotfiles/dot.vimrc" "$HOME/.vimrc"
+link_dotfile "$dotfiles/dot.zshenv" "$HOME/.zshenv"
+link_dotfile "$dotfiles/dot.zsh" "$HOME/.zsh"
